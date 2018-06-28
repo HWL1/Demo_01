0439a262629a836e5ae22f01902d6093dec6f5cd
HWL1 <38910201+HWL1@users.noreply.github.com>
2018-06-28 09:47:31 +0800

    Revert "Carpoolline"
    This reverts commit 66e5d6a6c7fa3bd9b7d19787a98b7686fad8fa74.

--- Carpoolline/src/main/java/com/Carpoolline/Passenger/User_registration/Dao/User_registration_mapper_face.java
+++
@@ -1,9 +0,0 @@
-package com.Carpoolline.Passenger.User_registration.Dao;
-
-import com.Carpoolline.Passenger.User_registration.Domain_Mapper.User_Passenger;
-
-public interface User_registration_mapper_face {
-	
-	public void Insert(User_Passenger user);
-
-}
